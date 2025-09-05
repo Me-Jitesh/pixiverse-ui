c2a419dc3568c9114a433d915cea30bc8786d285
Jitesh Singh <jiteshs101@gmail.com>
2025-09-05 22:49:13 +0530

    Identity added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="mt-12 mb-5">
-            <p className="footer-text">✨ Made with ❤️ by Jitesh ✨</p>
+            <p className="footer-text">✨ Made with ❤️ by Jitesh R Singh ✨</p>
         </footer>
     );
 }
